f615b2458e50ecf39e5729bcfb6566e728c9a68f
Bisma Reza Ferdian <bismarezaferdian95@gmail.com>
2024-01-11 21:14:25 +0700

    try fixing cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: "cloudinary",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        cloud_name: env("CLOUDINARY_NAME", "dcjmcruvj"),
+        api_key: env("CLOUDINARY_KEY", "493291779383164"),
+        api_secret: env("CLOUDINARY_SECRET", "v5JId55a0KK0HkiIKPuuzzNfFJw"),
       },
       actionOptions: {
         upload: {},
